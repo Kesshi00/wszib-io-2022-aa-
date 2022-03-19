2938eb77af63e35cba64028f2712f8730e331188
Tomasz Rojek <me@tomrosystems.com>
2022-03-20 03:13:43 +0800

    Pierwszy commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rozpoznawanie typu trójkąta
+Prototyp - nie zaleca się używać na produkcji.
+
+## Kompilowanie
+`javac trojkaty.java`
+
+## Uruchamianie
+`java trojkaty 1 2 3`
---
+++ trojkaty.java
@@ -0,0 +1,36 @@
+/**
+* To jest program do rozpoznawania trójkąta.
+*/
+
+class Trojkaty {
+    /**
+     * Główna fnukcja programu.
+     * @param {float} a - Długość pierwszego boku.
+     * @param {float} b - Długość drugiegio boku.
+     * @param {float} c - Długość trzeciego boku.
+     */
+    public static void jakiTrojkat(float a, float b, float c){
+         if (a == b && b == c && a == c) {
+             System.out.println("Trójkąt równoboczny"); 
+         }
+         // TODO: tutaj trzeba bedzie dopisac inne przypadki
+    }
+    /** Wyświetla ekran pomocy */
+    public static void pomoc(){
+        System.out.println("Acme INC. (C) 2022");
+        System.out.println("Program do rozpoznawania rodzaju trójkąra");
+        System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
+    }
+    /** Glowna funkcja */
+    public static void main(String... args) {
+        if (args.length != 3) {
+            pomoc();
+            System.exit(1);
+        }
+        float a = Float.valueOf(args[0]);
+        float b = Float.valueOf(args[1]);
+        float c = Float.valueOf(args[2]);
+        
+        jakiTrojkat(a, b, c);
+    }
+}

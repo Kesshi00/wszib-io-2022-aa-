16accd0d8d787314e7251a2fb5dfdd50f2c63a2a
MiLabuda <mlabuda544@gmail.com>
2022-07-02 23:29:57 +0200

    Nowa funckjonalność: Trójkąt równoramienny

--- trojkaty.java
+++ trojkaty.java
@@ -13,6 +13,12 @@ class Trojkaty {
          if (a == b && b == c && a == c) {
              System.out.println("Trójkąt równoboczny"); 
          }
+	if(a == b && b == c && a == c) {
+		System.out.println("Trójkąt równoboczny");
+}
+	if(a ==b || b == c || a == c) {
+		System.out.println("Trójkąt równoramienny");
+}
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
     /** Wyświetla ekran pomocy */

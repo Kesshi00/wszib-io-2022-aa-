521e0ef2829da4bca10fabf24dfd05ae7ad4e599
MiLabuda <mlabuda544@gmail.com>
2022-07-02 23:00:14 +0200

    Dział dokumentacji: Poprawa komentarzy w kodzie

--- trojkaty.java
+++ trojkaty.java
@@ -4,9 +4,9 @@
 
 class Trojkaty {
     /**
-     * Główna fnukcja programu.
+     * Główna funkcja programu.
      * @param {float} a - Długość pierwszego boku.
-     * @param {float} b - Długość drugiegio boku.
+     * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){

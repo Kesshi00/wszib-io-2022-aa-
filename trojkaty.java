6449b5eb3421cfd53c141aada38e4f89bebbfb7e
draxolini <draxolini@interia.pl>
2022-07-02 23:38:29 +0200

    Poprawa ekranu pomocy

--- trojkaty.java
+++ trojkaty.java
@@ -21,7 +21,7 @@ class Trojkaty {
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
-        System.out.println("Program do rozpoznawania rodzaju trójkąra");
+        System.out.println("Program do rozpoznawania rodzaju trójkąta");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
     /** Glowna funkcja */

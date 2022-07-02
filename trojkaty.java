9611b7f1a07053e5c057fa2960c2009b4a5fa7ee
MiLabuda <mlabuda544@gmail.com>
2022-07-02 23:19:13 +0200

    Dział optymalizacji: usunięcie niepotrzebnego kodu

--- trojkaty.java
+++ trojkaty.java
@@ -10,7 +10,7 @@ class Trojkaty {
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
-         if (a == b && b == c && a == c) {
+         if (a == b && b == c) {
              System.out.println("Trójkąt równoboczny"); 
          }
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
